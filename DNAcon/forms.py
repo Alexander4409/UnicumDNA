90caee0a65c71eb16d501a0ff4f19d3ebbb755af
Alexander <aleksandr.tailakov@mail.ru>
2023-10-04 14:48:41 +0700

    Upgrade registration V2

--- DNAcon/forms.py
+++ DNAcon/forms.py
@@ -1,15 +1,30 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
+    phone_number = forms.CharField(max_length=15, required=True)
+
+    # Определите список доступных групп
+    GROUP_CHOICES = (
+        ('ИП-64', 'ИП-64'),
+        ('ИП-65', 'ИП-65'),
+        ('Р-71', 'Р-71'),
+        # Добавьте другие группы по аналогии
+    )
+
+    # Используйте ChoiceField для выбора группы
+    group = forms.ChoiceField(choices=GROUP_CHOICES, required=True)
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2', 'phone_number', 'group']
 
         labels = {
             'first_name': 'Name',
             'last_name': 'Last Name',
+            'phone_number': 'Phone Number',
         }
 
     def __init__(self, *args, **kwargs):
@@ -17,3 +32,4 @@ class CustomUserCreationForm(UserCreationForm):
 
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'input'})
+
--- DNAcon/views.py
+++ DNAcon/views.py
@@ -1,10 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import Group
 from django.contrib.auth import login, logout, authenticate
-from django.db import IntegrityError
 from .forms import CustomUserCreationForm
+from .models import UserProfile
 # Create your views here.
 
 
@@ -21,6 +21,23 @@ def signupuser(request):
             user = form.save(commit=False)
             user.username = user.username.lower().strip()
             user.save()
+
+            # Проверяем, существует ли профиль пользователя, и создаем его, если нет
+            user_profile, created = UserProfile.objects.get_or_create(user=user)
+            user_profile.phone_number = form.cleaned_data['phone_number']
+            user_profile.save()
+
+            # Получаем выбранную группу из формы и добавляем пользователя в неё
+            group = form.cleaned_data['group']
+
+            # Пример: добавление пользователя в группу "ИП-64"
+            if group == 'ИП-64':
+                user.groups.add(Group.objects.get(name='ИП-64'))
+            # Пример: добавление пользователя в группу "ИП-65"
+            elif group == 'ИП-65':
+                user.groups.add(Group.objects.get(name='ИП-65'))
+            # Добавьте другие группы по аналогии
+
             messages.success(request, "User account was created!")
             login(request, user)
             return redirect('current')
@@ -29,8 +46,6 @@ def signupuser(request):
 
     context = {'page': page, 'form': form}
     return render(request, 'DNAcon/signupuser.html', context)
-
-
 
 
 def loginuser(request):
